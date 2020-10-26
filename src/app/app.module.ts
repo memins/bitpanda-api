bec4173bde67ca57d59dc9eeb21f38667b70b9bc
Muhammed Sahin <muhammed.sahin@acp.at>
2020-10-26 01:04:44 +0100

    added primeng on project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { MenubarModule } from 'primeng/menubar';
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, MenubarModule],
   providers: [],
   bootstrap: [AppComponent],
 })
